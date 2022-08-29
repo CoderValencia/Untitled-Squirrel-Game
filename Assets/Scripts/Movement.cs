15750b5f1349f32003aa42db1b60f6ff0131bb03
CoderValencia <valencia.coleman625@gmail.com>
2022-08-29 19:08:53 -0400

    Some work on Movement and start script of player health

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,20 +5,47 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float moveSpeed;
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Animator animator;
 
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
+    }
 
     void Update()
     {
-        moveInput.x = Input.GetAxisRaw("Horizontal");
-        moveInput.y = Input.GetAxisRaw("Vertical");
+        Move();
+        //Animate();
+        //moveInput.x = Input.GetAxisRaw("Horizontal");
+       // moveInput.y = Input.GetAxisRaw("Vertical");
+    }
+    
+    private void Move()
+    {
+        float Horizontal = Input.GetAxisRaw("Horizontal");
+        float Vertical = Input.GetAxisRaw("Vertical");
 
-        moveInput.Normalize();
+        if (Horizontal == 0 && Vertical == 0)
+        {
+            rb.velocity = new Vector2 (0, 0);
+            return;
+        }
+        moveInput = new Vector2(Horizontal, Vertical);
+        rb.velocity = moveInput * moveSpeed * Time.fixedDeltaTime;
+    }
+    private void Animate()
+    {
+        animator.SetFloat("MovementX", moveInput.x);
+        animator.SetFloat("MovementY", moveInput.y);
 
-        rb.velocity = moveInput * moveSpeed;
+    }
 
-
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
     }
 }
